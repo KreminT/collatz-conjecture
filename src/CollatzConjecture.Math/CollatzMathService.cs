3b4627c6f2f67ff646cb5976eeb5831e57da15f2
KreminT <kremin.t@gmail.com>
2023-01-28 15:42:32 +0200

    math3X

--- src/CollatzConjecture.Math/CollatzMathService.cs
+++ src/CollatzConjecture.Math/CollatzMathService.cs
@@ -1,4 +1,6 @@
-﻿namespace CollatzConjecture.Math
+﻿using CollatzConjecture.Math.Exception;
+
+namespace CollatzConjecture.Math
 {
     public class CollatzMathService : ICollatzMathService
     {
@@ -9,7 +11,28 @@
 
         public string Math3X(string number)
         {
-            throw new NotImplementedException();
+            if (!number.IsNumeric())
+                throw new NotNumericException(number);
+            string result = string.Empty;
+            int prevValue = 0;
+            List<string> numbers = number.SplitNumericToParts(3);
+            for (int i = numbers.Count - 1; i >= 0; i--)
+            {
+                int value = int.Parse(numbers[i]) * 3;
+                if (i == numbers.Count - 1)
+                    value += 1;
+                value += prevValue;
+                string val = value.ToString();
+                if (val.Length > numbers[i].Length)
+                    prevValue = int.Parse(val.Substring(0, val.Length - numbers[i].Length));
+                else
+                    prevValue = 0;
+                if (i == 0)
+                    result = result.Insert(0, val);
+                else
+                    result = result.Insert(0, val.Substring(val.Length - numbers[i].Length, numbers[i].Length));
+            }
+            return result;
         }
     }
 }
---
+++ test/CollatzConjecture.Math.Test/CollatzMathServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using CollatzConjecture.Math.Exception;
+using Xunit;
+
+namespace CollatzConjecture.Math.Test
+{
+    public class CollatzMathServiceTests
+    {
+        [Fact]
+        public void Math3XTest()
+        {
+            CollatzMathService mathService = new CollatzMathService();
+
+            Assert.Throws<NotNumericException>(()=>mathService.Math3X("48,974,897"));
+            Assert.Equal("146924692", mathService.Math3X("48974897"));
+            Assert.Equal("13327297298229729730", mathService.Math3X("4442432432743243243"));
+            Assert.Equal("22458979439497942177942096", mathService.Math3X("7486326479832647392647365"));
+        }
+    }
+}
